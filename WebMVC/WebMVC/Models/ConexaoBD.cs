6726b25aa87be3c65b30e1bfc52dcf1baa139fbf
Jose fernandes <zefrancisco2002@gmail.com>
2020-12-16 09:30:01 +0000

    Aula6PrimeiroTempo

---
+++ WebMVC/WebMVC/Models/ConexaoBD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+namespace WebMVC.Models
+{
+    public class ConexaoBD
+    {
+        private string host;
+        private int porta;
+        private string bd;
+        private string utilizador;
+        private string password;
+        private MySqlConnection conn = null;
+
+
+        public ConexaoBD(string host, int porta, string utilizador, string password, string bd)
+        {
+            this.host = host;
+            this.porta = porta;
+            this.utilizador = utilizador;
+            this.password = password;
+            this.bd = bd;
+        }
+
+        public MySqlConnection ObterConexao()
+        {
+            try
+            {
+                string connectionInfo = "datasource=" + host + ";port=" + porta + ";username=" + utilizador + ";password=" + password + ";database=" + bd + ";SslMode=none";
+                conn = new MySqlConnection(connectionInfo);
+                conn.Open();
+                return conn;
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+            }
+            return null;
+        }
+    }
+}
