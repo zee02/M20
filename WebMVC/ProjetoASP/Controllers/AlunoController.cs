4d99b0a2543035dbca3e9a977a1b53e35ee60e68
Jose fernandes <zefrancisco2002@gmail.com>
2021-01-07 15:03:30 +0000

    Changes

--- WebMVC/ProjetoASP/Controllers/AlunoController.cs
+++ WebMVC/ProjetoASP/Controllers/AlunoController.cs
@@ -100,7 +100,7 @@ namespace ProjetoASP.Controllers
 
                                 aluno = new Aluno()
                                 {
-                                    Naluno = reader.GetInt32("idAlunos"),
+                                    Naluno = reader.GetInt32("idalunos"),
                                     PriNome = reader.GetString("nome"),
                                     UltNome = reader.GetString("ultimo_nome"),
                                     Morada = reader.GetString("morada"),
@@ -117,5 +117,89 @@ namespace ProjetoASP.Controllers
             }
             return RedirectToAction("ListarAluno");
         }
+       
+        [HttpPost]
+        public ActionResult EditaAluno(Aluno aluno)
+        {
+            if(ModelState.IsValid)
+            {
+                bool img = false;
+                if(aluno.Imagem != null)
+                {
+                    string ImagemNome = Path.GetFileNameWithoutExtension(aluno.Imagem.FileName);
+                    string ImagemExt = Path.GetExtension(aluno.Imagem.FileName);
+
+                    ImagemNome = DateTime.Now.ToString("yyyyMMddHHmmss") + "-" + ImagemNome.Trim() + ImagemExt;
+                    aluno.ImgPath = @"\Content\Imagens\" + ImagemNome;
+
+                    aluno.Imagem.SaveAs(ControllerContext.HttpContext.Server.MapPath(aluno.ImgPath));
+                    img = true;
+                }
+                ConexaoDB conn = new ConexaoDB("localhost", 3307, "root", "root", "formacao");
+                using(MySqlConnection conexao = conn.ObterConexao())
+                {
+                    if(conexao != null)
+                    {
+                        string strFoto = (img) ? ",foto=@foto" : "";
+                        string stm = "update alunos set nome=@primeiroNome," + "ultimo_nome=@ultimoNome," + 
+                        "morada=@morada," + "genero=@genero," + "datanasc=@dataNasc," + "ano_escolaridade=@ano," + strFoto + " where idalunos=@idAluno";
+
+                        using (MySqlCommand cmd = new MySqlCommand(stm, conexao))
+                        {
+                            cmd.Parameters.AddWithValue("@primeiroNome", aluno.PriNome);
+                            cmd.Parameters.AddWithValue("@ultimoNome", aluno.UltNome);
+                            cmd.Parameters.AddWithValue("@morada", aluno.Morada);
+                            cmd.Parameters.AddWithValue("@genero", aluno.Genero);
+                            cmd.Parameters.AddWithValue("@dataNasc", aluno.DataNasc);
+                            cmd.Parameters.AddWithValue("@ano", aluno.AnoEscolaridade);
+                            cmd.Parameters.AddWithValue("@idAluno", aluno.Naluno);
+
+                            if (img)
+                                cmd.Parameters.AddWithValue("@foto", aluno.ImgPath);
+
+                                int nRgistos = cmd.ExecuteNonQuery();
+                               
+                            
+                        }   
+                }
+                }
+            }
+            return RedirectToAction("ListarAluno");
+        }
+        public ActionResult EditaAluno(int? id)
+        {
+            ConexaoDB Conn = new ConexaoDB("localhost", 3307, "root", "root", "formacao");
+            Aluno aluno = null;
+            using (MySqlConnection conexao = Conn.ObterConexao())
+            {
+                if (conexao != null)
+                    using (MySqlCommand cmd = new MySqlCommand("select * from alunos where idalunos=@idaluno", conexao))
+                    {
+                        cmd.Parameters.AddWithValue("@idaluno", id);
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+
+                                aluno = new Aluno()
+                                {
+                                    Naluno = reader.GetInt32("idalunos"),
+                                    PriNome = reader.GetString("nome"),
+                                    UltNome = reader.GetString("ultimo_nome"),
+                                    Morada = reader.GetString("morada"),
+                                    Genero = reader.GetString("genero") == "Masculino" ? Genero.Masculino : Genero.Feminino,
+                                    DataNasc = reader.GetDateTime("datanasc"),
+                                    AnoEscolaridade = reader.GetInt16("ano_escolaridade"),
+                                    ImgPath = reader.GetString("foto")
+                                };
+                                return View(aluno);
+                            }
+                        }
+                    }
+
+            }
+            return RedirectToAction("ListarAluno");
+        }
+
     }
 }
