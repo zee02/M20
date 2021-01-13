0d22ed53e4bd8098c9b3afda6e071103872f1f81
Jose fernandes <zefrancisco2002@gmail.com>
2021-01-13 09:29:36 +0000

    Changes

---
+++ MVCEntity/MVCEntity/Models/contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCEntity.Models
+{
+    public class contexto:DbContext
+    {
+        public DbSet<pessoa> pessoas { get; set; }
+    }
+}
---
+++ MVCEntity/MVCEntity/Models/pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEntity.Models
+{
+    public class pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
